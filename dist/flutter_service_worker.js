32a85f621de6a864d20a78f0438c0cd71f37ed83
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-12-30 15:30:29 +0100

    Add dist files for deployment

--- dist/flutter_service_worker.js
+++ dist/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a1c217ff08a16052ed9d682d105cf8e3",
+const RESOURCES = {"flutter_bootstrap.js": "2f346e67c3b7bac46ca94aeb191f9107",
 "version.json": "1d94fac6163a65b96ec236d547c0102b",
 "index.html": "d7e4d52f9ca805066a4cc86aab41863a",
 "/": "d7e4d52f9ca805066a4cc86aab41863a",
